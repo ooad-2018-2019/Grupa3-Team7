d5090cc24e9a8c7a97d76eae2f499758845bf0e4
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2019-06-07 18:16:59 +0200

    Added initial project implementation

---
+++ Implementation/WMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WMS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<>
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<>().ToTable("Student");
+        }
+    }
+}
