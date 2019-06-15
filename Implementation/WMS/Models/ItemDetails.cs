39f7ade5b56e357f8ac074607445684c0017143c
Tarik Sijerčić <tsijercic1@etf.unsa.ba>
2019-06-15 12:47:55 +0200

    Added ItemDetails, Item and StorageSpace models

---
+++ Implementation/WMS/Models/ItemDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WMS.Models
+{
+    public class ItemDetails
+    {
+        [Key]
+        public string UPC { get; set; }
+        public string Name { get; set; }
+        public double Volume { get; set; }
+        public string Description { get; set; }
+        public string ImageURI { get; set; }
+
+    }
+}
---
+++ Implementation/WMS/Models/StorageSpace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WMS.Models
+{
+    public class StorageSpace
+    {
+        public string Id  { get; set; }
+        public double Capacity { get; set; }
+        public double UsedUp { get { return 0; } }
+        public ICollection<Item> Items { get; set; }
+        public Firm Firm { get; set; }
+
+    }
+}
