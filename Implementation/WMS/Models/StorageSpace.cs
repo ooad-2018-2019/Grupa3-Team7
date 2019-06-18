32414cbffad2b04e3df6ed85373f6821c1991089
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2019-06-18 16:50:00 +0200

    Modified StorageSpace model

--- Implementation/WMS/Models/StorageSpace.cs
+++ Implementation/WMS/Models/StorageSpace.cs
@@ -10,10 +10,20 @@ namespace WMS.Models
     public class StorageSpace
     {
         public string Id  { get; set; }
+        public string Name { get; set; }
         public double Capacity { get; set; }
-        public double UsedUp { get { return 0; } }
-        public ICollection<Item> Items { get; set; }
+        public double UsedUp {
+            get {
+                double usedUp = 0;
+                foreach(Item item in Items) {
+                    usedUp += item.ItemDetails.Volume;
+                }
+                usedUp /= Capacity;
+                return usedUp;
+            }
+        }
 
+        public ICollection<Item> Items { get; set; }
         public Firm Firm{ get; set; }
     }
 }
