e0a60cdd8a07de4ba8f5c9c844820f32c5b22f75
Tarik Sijerčić <tsijercic1@etf.unsa.ba>
2019-06-18 17:23:51 +0200

    migration update

--- Implementation/WMS/Migrations/20190618152200_initialVol16.Designer.cs
+++ Implementation/WMS/Migrations/20190618152200_initialVol16.Designer.cs
@@ -10,14 +10,14 @@ using WMS.Models;
 namespace WMS.Migrations
 {
     [DbContext(typeof(WMSContext))]
-    [Migration("20190618100950_test")]
-    partial class test
+    [Migration("20190618152200_initialVol16")]
+    partial class initialVol16
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.2-servicing-10034")
+                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
@@ -145,6 +145,9 @@ namespace WMS.Migrations
                     b.Property<string>("ConcurrencyStamp")
                         .IsConcurrencyToken();
 
+                    b.Property<string>("Discriminator")
+                        .IsRequired();
+
                     b.Property<string>("Email")
                         .HasMaxLength(256);
 
@@ -184,6 +187,156 @@ namespace WMS.Migrations
                         .HasFilter("[NormalizedUserName] IS NOT NULL");
 
                     b.ToTable("AspNetUsers");
+
+                    b.HasDiscriminator<string>("Discriminator").HasValue("WMSUser");
+                });
+
+            modelBuilder.Entity("WMS.Models.Item", b =>
+                {
+                    b.Property<string>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ItemDetailsUPC");
+
+                    b.Property<string>("StorageSpaceId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("ItemDetailsUPC");
+
+                    b.HasIndex("StorageSpaceId");
+
+                    b.ToTable("Items");
+                });
+
+            modelBuilder.Entity("WMS.Models.ItemCount", b =>
+                {
+                    b.Property<int>("Count")
+                        .ValueGeneratedOnAdd()
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("ItemUPC");
+
+                    b.Property<string>("RequestId");
+
+                    b.HasKey("Count");
+
+                    b.HasIndex("ItemUPC");
+
+                    b.HasIndex("RequestId");
+
+                    b.ToTable("ItemCounts");
+                });
+
+            modelBuilder.Entity("WMS.Models.ItemDetails", b =>
+                {
+                    b.Property<string>("UPC")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("ImageURI");
+
+                    b.Property<string>("Name");
+
+                    b.Property<double>("Volume");
+
+                    b.HasKey("UPC");
+
+                    b.ToTable("ItemDetails");
+                });
+
+            modelBuilder.Entity("WMS.Models.Request", b =>
+                {
+                    b.Property<string>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Discriminator")
+                        .IsRequired();
+
+                    b.Property<string>("FirmId");
+
+                    b.Property<bool>("Processed");
+
+                    b.Property<DateTime>("RequestDate");
+
+                    b.Property<string>("StorageSpaceId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("FirmId");
+
+                    b.HasIndex("StorageSpaceId");
+
+                    b.ToTable("Requests");
+
+                    b.HasDiscriminator<string>("Discriminator").HasValue("Request");
+                });
+
+            modelBuilder.Entity("WMS.Models.StorageSpace", b =>
+                {
+                    b.Property<string>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<double>("Capacity");
+
+                    b.Property<string>("FirmId");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("FirmId");
+
+                    b.HasIndex("Name");
+
+                    b.ToTable("StorageSpaces");
+                });
+
+            modelBuilder.Entity("WMS.Models.Warehouse", b =>
+                {
+                    b.Property<string>("Name")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<double>("Capacity");
+
+                    b.HasKey("Name");
+
+                    b.ToTable("Warehouses");
+                });
+
+            modelBuilder.Entity("WMS.Models.Employee", b =>
+                {
+                    b.HasBaseType("WMS.Areas.Identity.Data.WMSUser");
+
+                    b.Property<string>("FullName");
+
+                    b.Property<DateTime>("HireDate");
+
+                    b.HasDiscriminator().HasValue("Employee");
+                });
+
+            modelBuilder.Entity("WMS.Models.Firm", b =>
+                {
+                    b.HasBaseType("WMS.Areas.Identity.Data.WMSUser");
+
+                    b.Property<string>("FirmName");
+
+                    b.HasDiscriminator().HasValue("Firm");
+                });
+
+            modelBuilder.Entity("WMS.Models.ExportRequest", b =>
+                {
+                    b.HasBaseType("WMS.Models.Request");
+
+                    b.HasDiscriminator().HasValue("ExportRequest");
+                });
+
+            modelBuilder.Entity("WMS.Models.ImportRequest", b =>
+                {
+                    b.HasBaseType("WMS.Models.Request");
+
+                    b.HasDiscriminator().HasValue("ImportRequest");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
@@ -229,6 +382,50 @@ namespace WMS.Migrations
                         .WithMany()
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("WMS.Models.Item", b =>
+                {
+                    b.HasOne("WMS.Models.ItemDetails", "ItemDetails")
+                        .WithMany()
+                        .HasForeignKey("ItemDetailsUPC");
+
+                    b.HasOne("WMS.Models.StorageSpace")
+                        .WithMany("Items")
+                        .HasForeignKey("StorageSpaceId");
+                });
+
+            modelBuilder.Entity("WMS.Models.ItemCount", b =>
+                {
+                    b.HasOne("WMS.Models.ItemDetails", "Item")
+                        .WithMany()
+                        .HasForeignKey("ItemUPC");
+
+                    b.HasOne("WMS.Models.Request")
+                        .WithMany("Items")
+                        .HasForeignKey("RequestId");
+                });
+
+            modelBuilder.Entity("WMS.Models.Request", b =>
+                {
+                    b.HasOne("WMS.Models.Firm", "Firm")
+                        .WithMany()
+                        .HasForeignKey("FirmId");
+
+                    b.HasOne("WMS.Models.StorageSpace", "StorageSpace")
+                        .WithMany()
+                        .HasForeignKey("StorageSpaceId");
+                });
+
+            modelBuilder.Entity("WMS.Models.StorageSpace", b =>
+                {
+                    b.HasOne("WMS.Models.Firm", "Firm")
+                        .WithMany("StorageSpaces")
+                        .HasForeignKey("FirmId");
+
+                    b.HasOne("WMS.Models.Warehouse")
+                        .WithMany("StorageSpaces")
+                        .HasForeignKey("Name");
                 });
 #pragma warning restore 612, 618
         }
--- Implementation/WMS/Migrations/20190618152200_initialVol16.cs
+++ Implementation/WMS/Migrations/20190618152200_initialVol16.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace WMS.Migrations
 {
-    public partial class Initial : Migration
+    public partial class initialVol16 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -40,11 +40,42 @@ namespace WMS.Migrations
                     TwoFactorEnabled = table.Column<bool>(nullable: false),
                     LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
                     LockoutEnabled = table.Column<bool>(nullable: false),
-                    AccessFailedCount = table.Column<int>(nullable: false)
+                    AccessFailedCount = table.Column<int>(nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    FullName = table.Column<string>(nullable: true),
+                    HireDate = table.Column<DateTime>(nullable: true),
+                    FirmName = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemDetails",
+                columns: table => new
+                {
+                    UPC = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Volume = table.Column<double>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    ImageURI = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemDetails", x => x.UPC);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Warehouses",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false),
+                    Capacity = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Warehouses", x => x.Name);
                 });
 
             migrationBuilder.CreateTable(
@@ -153,6 +184,111 @@ namespace WMS.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "StorageSpaces",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Capacity = table.Column<double>(nullable: false),
+                    FirmId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StorageSpaces", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StorageSpaces_AspNetUsers_FirmId",
+                        column: x => x.FirmId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_StorageSpaces_Warehouses_Name",
+                        column: x => x.Name,
+                        principalTable: "Warehouses",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    ItemDetailsUPC = table.Column<string>(nullable: true),
+                    StorageSpaceId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Items_ItemDetails_ItemDetailsUPC",
+                        column: x => x.ItemDetailsUPC,
+                        principalTable: "ItemDetails",
+                        principalColumn: "UPC",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Items_StorageSpaces_StorageSpaceId",
+                        column: x => x.StorageSpaceId,
+                        principalTable: "StorageSpaces",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    RequestDate = table.Column<DateTime>(nullable: false),
+                    FirmId = table.Column<string>(nullable: true),
+                    Processed = table.Column<bool>(nullable: false),
+                    StorageSpaceId = table.Column<string>(nullable: true),
+                    Discriminator = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requests_AspNetUsers_FirmId",
+                        column: x => x.FirmId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Requests_StorageSpaces_StorageSpaceId",
+                        column: x => x.StorageSpaceId,
+                        principalTable: "StorageSpaces",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemCounts",
+                columns: table => new
+                {
+                    Count = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ItemUPC = table.Column<string>(nullable: true),
+                    RequestId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemCounts", x => x.Count);
+                    table.ForeignKey(
+                        name: "FK_ItemCounts_ItemDetails_ItemUPC",
+                        column: x => x.ItemUPC,
+                        principalTable: "ItemDetails",
+                        principalColumn: "UPC",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ItemCounts_Requests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Requests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
             migrationBuilder.CreateIndex(
                 name: "IX_AspNetRoleClaims_RoleId",
                 table: "AspNetRoleClaims",
@@ -191,6 +327,46 @@ namespace WMS.Migrations
                 column: "NormalizedUserName",
                 unique: true,
                 filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemCounts_ItemUPC",
+                table: "ItemCounts",
+                column: "ItemUPC");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemCounts_RequestId",
+                table: "ItemCounts",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_ItemDetailsUPC",
+                table: "Items",
+                column: "ItemDetailsUPC");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_StorageSpaceId",
+                table: "Items",
+                column: "StorageSpaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_FirmId",
+                table: "Requests",
+                column: "FirmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_StorageSpaceId",
+                table: "Requests",
+                column: "StorageSpaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StorageSpaces_FirmId",
+                table: "StorageSpaces",
+                column: "FirmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StorageSpaces_Name",
+                table: "StorageSpaces",
+                column: "Name");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -211,10 +387,28 @@ namespace WMS.Migrations
                 name: "AspNetUserTokens");
 
             migrationBuilder.DropTable(
+                name: "ItemCounts");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
                 name: "AspNetRoles");
 
             migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.DropTable(
+                name: "ItemDetails");
+
+            migrationBuilder.DropTable(
+                name: "StorageSpaces");
+
+            migrationBuilder.DropTable(
                 name: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Warehouses");
         }
     }
 }
